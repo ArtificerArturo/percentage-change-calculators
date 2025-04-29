8bf10ac18ea64b155125a3957dae3a6c089ef703
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-04-29 11:36:20 -0500

    Reworked due to feedback

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 function calculatePercentChange() {
-   const numberInput = document.querySelector("#percentChange #wholeInput")
-   const percentInput = document.querySelector("#percentChange #percentInput")
-   const dropdown = document.querySelector("#percentChange #dropdown")
+   const initialInput = document.querySelector("#percentChange #initialInput")
+   const finalInput = document.querySelector("#percentChange #finalInput")
    const backgroundElement = document.querySelector("#percentChange .background")
 
-   let number = parseFloat(numberInput.value)
-   let percent = parseFloat(percentInput.value)
+   let initial = parseFloat(initialInput.value)
+   let final = parseFloat(finalInput.value)
    let result = 0
-
-   if (dropdown.value == "increased") {
-      result = number * (percent / 100 + 1)
-   } else if (dropdown.value == "decreased") {
-      result = number * (1 - percent / 100)
-   }
-   console.log(result)
+   let direction = ""
 
    if (document.querySelector("#percentChange .result")) {
       document.querySelector("#percentChange .result").remove()
    }
 
-   if (isNaN(number) || isNaN(percent)) return
+   if (isNaN(initial) || isNaN(final)) return
 
    let resultElement = document.createElement("div")
    resultElement.setAttribute("class", "result")
 
-   if (isNaN(result)) {
-      resultElement.innerHTML = `Answer: <strong>Undefined</strong>.`
+   if (final > initial) {
+      direction = "increase"
+      result = ((final - initial) / initial) * 100
+   } else if (initial > final) {
+      direction = "decrease"
+      result = (final / initial - 1) * -100
+   } else if (initial == final) {
+      direction = "increase"
+      result = 0
+   }
+
+   if (isNaN(result) || result == "Infinity") {
+      resultElement.innerHTML = `Answer: <strong>Undefined</strong>`
    } else {
-      resultElement.innerHTML = `Answer: ${resultConditioner(number)} ${dropdown.value} by ${resultConditioner(
-         percent
-      )}% is <strong>${resultConditioner(result)}</strong>.`
+      resultElement.innerHTML = `Answer: ${resultConditioner(final)} is a <strong>${resultConditioner(
+         result
+      )}%</strong> ${direction} from ${resultConditioner(initial)}`
    }
    backgroundElement.appendChild(resultElement)
 }
